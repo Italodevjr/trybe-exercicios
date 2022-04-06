45c2283b4be39bf1d50ede2d8f31cb084e7f2894
Ítalo Araújo <italoarauju2019@gmail.com>
2022-04-06 19:00:55 -0300

    Exercicios 1,2 e 3 respondidos

---
+++ fundamentos/bloco-4-instroducao-javaScript-logica-de-programacao/dia-1-javaScript-primeiros-passos/script.js
@@ -0,0 +1,28 @@
+const a = 10;
+const b = 20;
+
+let soma = a + b;
+let subtracao = a - b;
+let mult = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+//Retornando o maior valor de dois números.
+if (a > b){
+    console.log("A é maior que B")
+}else{
+    console.log("B é maior que A")
+}
+
+//Retornando o maior valor de três números.
+const c = 80;
+const d = 70;
+const e = 40;
+
+if(c > d && c > e){
+    console.log("C é o maior numero")
+}else if(d > e && d > c){
+    console.log("D é o maior numero")
+}else{
+    console.log("E é o maior numero")
+}
