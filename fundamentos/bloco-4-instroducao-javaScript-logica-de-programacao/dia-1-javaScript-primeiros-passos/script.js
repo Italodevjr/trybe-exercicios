b802c03abee9cc6cd4e63970c245ed335d281ad9
Ítalo Araújo <italoarauju2019@gmail.com>
2022-04-06 19:05:39 -0300

    4 questão resolvida

--- fundamentos/bloco-4-instroducao-javaScript-logica-de-programacao/dia-1-javaScript-primeiros-passos/script.js
+++ fundamentos/bloco-4-instroducao-javaScript-logica-de-programacao/dia-1-javaScript-primeiros-passos/script.js
@@ -1,20 +1,20 @@
 const a = 10;
 const b = 20;
 
-let soma = a + b;
-let subtracao = a - b;
-let mult = a * b;
-let divisao = a / b;
-let modulo = a % b;
+console.log('Soma: ' + (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação: ' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Módulo: ' + (a % b));
 
-//Retornando o maior valor de dois números.
+//2.Retornando o maior valor de dois números.
 if (a > b){
     console.log("A é maior que B")
 }else{
     console.log("B é maior que A")
 }
 
-//Retornando o maior valor de três números.
+//3.Retornando o maior valor de três números.
 const c = 80;
 const d = 70;
 const e = 40;
@@ -25,4 +25,15 @@ if(c > d && c > e){
     console.log("D é o maior numero")
 }else{
     console.log("E é o maior numero")
+}
+
+//4.Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+const numero = 10;
+
+if(numero > 0){
+    console.log("Positivo");
+}else if(numero < 0){
+    console.log("Negativo");
+}else{
+    console.log("Zero");
 }
