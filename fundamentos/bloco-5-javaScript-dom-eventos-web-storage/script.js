05f4f76a3ab788992df993891be4fa5a31bebe58
Ítalo Araújo <italoarauju2019@gmail.com>
2022-04-19 21:13:32 -0300

    Adicionado clear, font, espaçamentos entre linhas

--- fundamentos/bloco-5-javaScript-dom-eventos-web-storage/script.js
+++ fundamentos/bloco-5-javaScript-dom-eventos-web-storage/script.js
@@ -16,10 +16,10 @@ function corDeFundo() {
 corDeFundo()
 
 
+let textoP = document.querySelector (".textoP")
 function corTexto (){
 
 let texto = document.querySelector ("#texto")
-let textoP = document.querySelector (".textoP")
     texto.addEventListener("change", function (color){
         color = texto.value;
         textoP.style.color = color;
@@ -31,3 +31,43 @@ let textoP = document.querySelector (".textoP")
 }
 
 corTexto()
+
+function tamanhoSize (){
+    let font = document.querySelector ("#font")
+    
+    font.addEventListener("change", function(size){
+        size = font.value;
+        textoP.style.fontSize = size
+        localStorage.setItem("tamanho", size)
+    }) 
+
+    let tamanhoSalved = localStorage.getItem("tamanho")
+    textoP.style.fontSize = tamanhoSalved
+}
+
+tamanhoSize()
+
+function espacoLine (){
+
+    let espaco = document.querySelector ("#espaco")
+    espaco.addEventListener("change", function(line){
+        line = espaco.value;
+        textoP.style.lineHeight = line
+        localStorage.setItem("espacoLine", line)
+    }) 
+
+    let tamanhoSalved = localStorage.getItem("espacoLine")
+    textoP.style.lineHeight = tamanhoSalved
+}
+
+espacoLine()
+
+function clear (){
+let clear = document.querySelector("#clear")
+
+clear.addEventListener("click", function (){
+    return localStorage.clear()  
+})
+
+}
+clear()
